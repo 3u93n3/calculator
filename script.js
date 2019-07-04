3e9b00aab04af379d42a5e1fa3fdf9df3e5caf70
Eugene <44581767+3u93n3@users.noreply.github.com>
2019-07-04 14:25:42 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,129 +1,143 @@
-var newCalc = new Calculator;
+var c1 = new Calculator;
 
 var disp = document.getElementById('display');
-var number1 = 0, number2 = 0, input = '', operator;
+var number1 = 0, number2 = 0, input , operator;
 document.addEventListener('click', function(e){
-  disp.innerHTML = input;
-
-  newCalc.num(e.target.id);
-  console.log(e.target.id);
-  // switch(e.target.id){
+  disp.innerHTML = input; 
+  
+  switch(e.target.id){
     
-  //   //Numbers
-  //   case '1':
-  //     disp.innerHTML = input += 1;
-  //     break;
+    //Numbers
+    case '1':
+      c1.num(1);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '2':      
-  //     disp.innerHTML = input += 2;
-  //     break;
+    case '2':      
+      c1.num(2);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '3':      
-  //     disp.innerHTML = input += 3;
-  //     break;
+    case '3':      
+      c1.num(3);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '4':      
-  //     disp.innerHTML = input += 4;
-  //     break;
+    case '4':      
+      c1.num(4);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '5':      
-  //     disp.innerHTML = input += 5;
-  //     break;
+    case '5':      
+      c1.num(5);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '6':      
-  //     disp.innerHTML = input += 6;
-  //     break;
+    case '6':      
+      c1.num(6);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '7':      
-  //     disp.innerHTML = input += 7;
-  //     break;
+    case '7':      
+      c1.num(7);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '8':      
-  //     disp.innerHTML = input += 8;
-  //     break;
+    case '8':      
+      c1.num(8);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '9':      
-  //     disp.innerHTML = input += 9;
-  //     break;
+    case '9':      
+      c1.num(9);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   case '0':      
-  //     disp.innerHTML = input += 0;
-  //     break;
+    case '0':      
+      c1.num(0);
+      if(c1.num2 != 0){
+        disp.innerHTML = parseFloat(c1.num2);
+      }else{
+        disp.innerHTML = parseFloat(c1.num1);
+      }
+      break;
 
-  //   //case 'point':      
-  //     //disp.innerHTML = input += '.';
-  //     //break;
+    case 'point':      
+      c1.num('.');
+      break;
 
-  //   //Operation
-  //   case 'add':
-  //     if(number1 == 0){
-  //       number1 = Number(input);   
-  //     }   
-  //     input = "";
-  //     operator = "+";
-  //     break;
+    //Operation
+    case 'add':
+      c1.num("+");
+      break;
 
-  //   case 'sub':
-  //     if(number1 == 0){
-  //       number1 = Number(input);  
-  //     }   
-  //     input = "";
-  //     operator = "-";
-  //     break;
+    case 'sub':
+      c1.num("-");
+      break;
 
-  //   case 'multi':
-  //     if(number1 == 0){
-  //       number1 = Number(input);      
-  //     }   
-  //     input = "";
-  //     operator = "*";
-  //     break;
+    case 'multi':
+      c1.num("*");
+      break;
 
-  //   case 'div':
-  //     if(number1 == 0){
-  //       number1 = Number(input);   
-  //     }   
-  //     input = "";
-  //     operator = "/";
-  //     break;
+    case 'div':
+      c1.num("/");
+      break;
 
-  //   //Equal
-  //   case 'equ':          
-  //     number2 = Number(input);  
-  //     input = 0;
-  //     switch(operator){
-  //       case "+":          
-  //         disp.innerHTML = number1 + number2;
-  //         number1 = number2 = 0;
-  //         break;
-  //       case "-":
-  //         disp.innerHTML = number1 - number2;
-  //         number1 = number2 = 0;
-  //         break;
-  //       case "*":
-  //         disp.innerHTML = number1 * number2;
-  //         number1 = number2 = 0;
-  //         break;
-  //       case "/":
-  //         disp.innerHTML = number1 / number2;
-  //         number1 = number2 = 0;
-  //         break;        
-  //     }    
-  //     break;
+    //Equal
+    case 'equ':          
+      c1.num("=");    
+      disp.innerHTML = parseFloat(c1.result);
+      break;
 
-  //   case 'clr':    
-  //     number1 = number2 = 0;       
-  //     input = '';
-  //     disp.innerHTML = '0';
-  //     break;
+    case 'clr':    
+      number1 = number2 = 0;       
+      input = '';
+      disp.innerHTML = '0';
+      break;
 
-  //   default:
-  //     disp.innerHTML = 0;
-  //     console.log("No such button");
-  // }
+    default:
+      disp.innerHTML = 0;
+      console.log("No such button");
+  }
 
-  //   console.log("Number1 = " + number1 + ", Number2 = " + number2 
-  //   +  ", input = " + input + ", operator = " + operator);
+    // console.log("Number1 = " + number1 + ", Number2 = " + number2 
+    // +  ", input = " + input + ", operator = " + operator);
   
 });
 
